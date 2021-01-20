052d421d50459d7def6701594cb1dad40275aff1
krButani <butanikartik1108@gmail.com>
2021-01-20 16:50:31 +0530

    Commit

---
+++ app/src/main/java/com/the/players/club/preventtakingscreenshot/MainActivity.java
@@ -0,0 +1,18 @@
+package com.the.players.club.preventtakingscreenshot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        }
+    }
+}
